676e690bb205e3f3e4c5d66483aba0f1d2755b2f
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2018-06-23 19:05:51 -0500

    test

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
   return app.send_static_file('index.html')
+
+@app.route('/icons')
+def 
 
 if __name__ == '__main__':
   app.run()
