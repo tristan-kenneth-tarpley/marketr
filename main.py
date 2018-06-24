02a93d6acc633f36b3a6236759242b0cd5794eb5
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2018-06-23 19:08:06 -0500

    test

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return app.send_static_file('index.html')
+	return app.send_static_file('index.html')
 
 @app.route('/icons')
-def 
+def icons():
+	return app.send_static_file('icons.html')
 
 if __name__ == '__main__':
-  app.run()
+	app.run()
