7bedcba1365b20043dc4f6620fbc2d3dc12e827a
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2018-06-26 10:03:52 -0500

    asdf

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, flash, request, url_for, redirect
 import numpy as np
 #import pandas as pd
-# import requests
+import requests
 # import pyodbc
 # import json
 # import colors
