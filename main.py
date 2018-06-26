0a5f95bd56501140128b8492879925ece7c068ed
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2018-06-25 21:41:35 -0500

    adsf

--- main.py
+++ main.py
@@ -1,75 +1,75 @@
-# #import analysis as an
-# from flask import Flask, render_template, flash, request, url_for, redirect
-# #import numpy as np
+#import analysis as an
+from flask import Flask, render_template, flash, request, url_for, redirect
+#import numpy as np
 
 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
 
-# # @app.route('/onload')
-# # def onload():
-# # 	a = an.get_customers()
-# # 	name = a['first_name'][0]
-# # 	last = a['last_name'][0]
-# # 	full_name = name + " " + last
-# # 	return full_name
+# @app.route('/onload')
+# def onload():
+# 	a = an.get_customers()
+# 	name = a['first_name'][0]
+# 	last = a['last_name'][0]
+# 	full_name = name + " " + last
+# 	return full_name
 
 
-# @app.route('/')
-# def index():
-# 	return app.send_static_file('index.html')
+@app.route('/')
+def index():
+	return app.send_static_file('index.html')
 
 
-# # @app.route('/dashboard')
-# # def dashboard():
-# # 	return app.send_static_file('dashboard.html')
+# @app.route('/dashboard')
+# def dashboard():
+# 	return app.send_static_file('dashboard.html')
 
 
 
-# # @app.route('/login/', methods=['POST', 'GET'])
-# # def login_page():
-# # 	error = ''
+# @app.route('/login/', methods=['POST', 'GET'])
+# def login_page():
+# 	error = ''
 
-# # 	if request.method == "POST":
+# 	if request.method == "POST":
 
-# # 		attempted_username = request.form['username']
-# # 		attempted_password = request.form['password']
+# 		attempted_username = request.form['username']
+# 		attempted_password = request.form['password']
 
-# # 		x = an.test()
+# 		x = an.test()
 
-# # 		#flash(attempted_username)
-# # 		#flash(attempted_password)
+# 		#flash(attempted_username)
+# 		#flash(attempted_password)
 
-# # 		if attempted_username == "admin" and attempted_password == "password":
-# # 		    return redirect(url_for('dashboard'))
+# 		if attempted_username == "admin" and attempted_password == "password":
+# 		    return redirect(url_for('dashboard'))
 			
-# # 		else:
-# # 		    error = "Invalid credentials. Try Again."
+# 		else:
+# 		    error = "Invalid credentials. Try Again."
 
 
-# # 	return redirect(url_for('dashboard'))
-# # 	#return render_template("login.html", error = error)
+# 	return redirect(url_for('dashboard'))
+# 	#return render_template("login.html", error = error)
 
 
 
-# @app.route('/recommendations')
-# def recommendations():
-# 	return app.send_static_file('recommendations.html')
+@app.route('/recommendations')
+def recommendations():
+	return app.send_static_file('recommendations.html')
 
-# @app.route('/campaigns')
-# def campaigns():
-# 	return app.send_static_file('campaigns.html')
+@app.route('/campaigns')
+def campaigns():
+	return app.send_static_file('campaigns.html')
 
-# @app.route('/integrations')
-# def integrations():
-# 	return app.send_static_file('integrations.html')
+@app.route('/integrations')
+def integrations():
+	return app.send_static_file('integrations.html')
 
-# @app.route('/settings')
-# def settings():
-# 	return app.send_static_file('settings.html')
+@app.route('/settings')
+def settings():
+	return app.send_static_file('settings.html')
 
 
-# if __name__ == '__main__':
-# 	app.run()
+if __name__ == '__main__':
+	app.run()
