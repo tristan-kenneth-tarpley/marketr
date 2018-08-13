276479b77553e6a697a3db242d9997a34fca76a1
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2018-08-13 18:09:06 -0500

    changed db credentials

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[13]:
+# In[1]:
 
 
 import pandas as pd
@@ -16,13 +16,13 @@ from watson_developer_cloud import ToneAnalyzerV3
 from itertools import product
 
 
-# In[14]:
+# In[3]:
 
 
-server = 'darbly.database.windows.net'
-database = 'blendo'
-username = 'tarpley'
-password = 'Password123!'
+server = 'tarpley.database.windows.net'
+database = 'marketr'
+username = 'tristan'
+password = 'Fiverrtemp!'
 driver= '{ODBC Driver 13 for SQL Server}'
 
 connStr = 'DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password
@@ -30,82 +30,56 @@ db = pyodbc.connect(connStr)
 cursor = db.cursor()
 
 
-# ## CONNECT DB
-
-# In[15]:
+# In[4]:
 
 
 def sql_to_df(x):
     return pd.read_sql_query(x, db, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
-
-
-# In[16]:
-
 
 def get_customers():
     query = "SELECT * FROM dbo.customers"
     a = sql_to_df(query)
     return a
 
-
-
-
-
-# In[18]:
-
-
 def debug(strSQL):
     print(strSQL)
 
 
-# ## API KEYS
-# Initializing APIs and updating database with attributes
-
-# In[19]:
+# In[5]:
 
 
 vision_subscription_key = '8359f1b134914ab1a1137c5a1d9d547c'
 assert vision_subscription_key
 
-
-# In[20]:
-
-
 text_subscription_key = '45f31e1a295e414b8d71319b79a405e0'
 assert text_subscription_key
-
-
-# ### Vision api
-
-# In[21]:
-
 
 vision_base_url = "https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/"
 vision_analyze_url = vision_base_url + "analyze"
 image_url = 'https://images.wagwalkingweb.com/media/breed/dachshund/appearance/dachshund.png?auto=compress&fit=max'
 
-
-# In[22]:
-
-
 vision_headers  = {'Ocp-Apim-Subscription-Key': vision_subscription_key }
 vision_params   = {'visualFeatures': 'Categories,Description,Color'}
 
 
-# ### Clean up
-
-# ### Populate Image Attributes DB
-
-# In[23]:
+# In[7]:
 
 
 def img_attr_to_memory():
     image_attr_query = "select * from dbo.image_attributes as dboi"
     image_attr_df = sql_to_df(image_attr_query)
     image_attr_df = image_attr_df.drop_duplicates(subset=['image_hash'], keep='last')
+    
+    return image_attr_df
 
 
-# In[24]:
+# In[8]:
+
+
+image_attr_df = img_attr_to_memory()
+
+
+# In[9]:
 
 
 def get_img_attr(img, row_select):
@@ -256,7 +230,7 @@ def get_img_attr(img, row_select):
     return update_statement
 
 
-# In[25]:
+# In[10]:
 
 
 def update_img_attr(df):
@@ -280,11 +254,7 @@ def update_img_attr(df):
         continue
 
 
-# # Update text attributes DB
-
-# ## Clean up
-
-# In[26]:
+# In[11]:
 
 
 text_attr_query = "select * from dbo.text_attributes as dboi"
@@ -293,36 +263,13 @@ text_attr_df = text_attr_df.dropna(subset=["body"], how='any')
 text_attr_df = text_attr_df.loc[660:]
 
 
-# In[27]:
-
-
-text_attr_df.shape
-
-
-# ### TEXT AI API
-
-# In[28]:
+# In[16]:
 
 
 text_analytics_base_url = "https://southcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/"
 language_api_url = text_analytics_base_url + "languages"
-
-
-
-# ### SENTIMENT
-
-# In[29]:
-
-
 sentiment_api_url = text_analytics_base_url + "sentiment"
-
-
-
-# ## Watson emotional analysis
-
-# In[30]:
-
-
+key_phrase_api_url = text_analytics_base_url + "keyPhrases"
 tone_analyzer = ToneAnalyzerV3(
     version ='2017-09-21',
     username ='e4a9a55c-764e-4799-8c00-b0886bb6f019',
@@ -330,33 +277,7 @@ tone_analyzer = ToneAnalyzerV3(
 )
 
 
-# {'document_tone': {'tones': [{'score': 0.855712,
-#                               'tone_id': 'confident',
-#                               'tone_name': 'Confident'}]},
-#  'sentences_tone': [{'sentence_id': 0,
-#                      'text': 'Through it all, I am optimistic.',
-#                      'tones': [{'score': 0.942582,
-#                                 'tone_id': 'confident',
-#                                 'tone_name': 'Confident'}]},
-#                     {'sentence_id': 1,
-#                      'text': 'But we have to get our shit together.',
-#                      'tones': [{'score': 0.637978,
-#                                 'tone_id': 'anger',
-#                                 'tone_name': 'Anger'},
-#                                {'score': 0.866237,
-#                                 'tone_id': 'confident',
-#                                 'tone_name': 'Confident'}]},
-#                     {'sentence_id': 2,
-#                      'text': 'We have a superior product, we just are not '
-#                              'selling it well enough!',
-#                      'tones': [{'score': 0.589295,
-#                                 'tone_id': 'analytical',
-#                                 'tone_name': 'Analytical'},
-#                                {'score': 0.716301,
-#                                 'tone_id': 'tentative',
-#                                 'tone_name': 'Tentative'}]}]}
-
-# In[31]:
+# In[13]:
 
 
 def get_emotional(text, row_select):
@@ -414,13 +335,7 @@ def get_emotional(text, row_select):
                 break
 
 
-# In[32]:
-
-
-counter = 0
-
-
-# In[33]:
+# In[14]:
 
 
 def update_emotions():
@@ -432,36 +347,7 @@ def update_emotions():
         get_emotional(body_copy, row['id'])
 
 
-# ## Last Index is 838, len is 597
-
-# ### KEY PHRASES
-
-# In[34]:
-
-
-key_phrase_api_url = text_analytics_base_url + "keyPhrases"
-
-
-# In[36]:
-
-
-# text_doc = {'documents' : [
-#     { 'id': '1', 'language': 'en', 'text': 'The quick brown fox jumped over the lazy sheepdog' },
-# ]}
-
-# key_phrases_headers   = {"Ocp-Apim-Subscription-Key": text_subscription_key}
-# key_phrases_response  = requests.post(key_phrase_api_url, headers=key_phrases_headers, json=text_doc)
-# key_phrases = key_phrases_response.json()
-
-
-# # In[37]:
-
-
-# t = key_phrases['documents'][0]['keyPhrases'][1]
-# len(key_phrases['documents'][0])
-
-
-# In[38]:
+# In[17]:
 
 
 def get_key_phrases(text, row_select, err):
@@ -526,7 +412,7 @@ def get_key_phrases(text, row_select, err):
     return update_statement
 
 
-# In[39]:
+# In[18]:
 
 
 def update_key_phrases_attr():
@@ -552,9 +438,7 @@ def update_key_phrases_attr():
             sleep(60.0 - ((time() - starttime) % 60.0))
 
 
-# ##############
-
-# In[40]:
+# In[19]:
 
 
 def get_sentiment_attr(text, row_select, err):
@@ -583,7 +467,7 @@ def get_sentiment_attr(text, row_select, err):
     return update_statement
 
 
-# In[41]:
+# In[20]:
 
 
 def update_text_attr():
@@ -606,27 +490,7 @@ def update_text_attr():
             sleep(60.0 - ((time() - starttime) % 60.0))
 
 
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# # Let the analysis...........BEGIN
-# 
-# Attributes:
-# - accent color, primary back, primary fore
-# - Cost Per Acquisition (from insights_actions)
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-
-# ### Get action stats
-
-# In[42]:
+# In[23]:
 
 
 actions_query = """
@@ -648,41 +512,13 @@ actions_query = """
 actions_df = sql_to_df(actions_query)
 
 
-# In[43]:
-
-
-
-
-# # In[44]:
-
-
-# actions_df.shape
-
-
-# # In[45]:
-
-
-# actions_df['ad_id'].value_counts().count()
-
-
-# ### actions stats (mean, std, count)
-
-# In[46]:
+# In[24]:
 
 
 actions_df['cpa'] = actions_df[['cost_per_10_sec_video_view_value', 'cpc', 'cpp', 'cpm']].mean(axis=1)
 
 
-# In[47]:
-
-
-actions_df.head(10)
-actions_df.shape
-
-
-# ### Merge all attributes with actions
-
-# In[48]:
+# In[25]:
 
 
 attr_query = """SELECT ia.*, ta.*, body, image_url, ads.id as ad_id
@@ -692,66 +528,21 @@ attr_query = """SELECT ia.*, ta.*, body, image_url, ads.id as ad_id
                 WHERE  ta.creative_id = ia.creative_id
                    AND ads.creative_id = ta.creative_id
                    AND ads.creative_id = ia.creative_id"""
-#debug(attr_query)
 attr_df = sql_to_df(attr_query)
 
 
-# In[49]:
-
-
-attr_df['ad_id'].value_counts().count()
-
-
-# In[50]:
+# In[26]:
 
 
 actions_attr_df = pd.merge(actions_df, attr_df, on='ad_id')
-
-
-# In[51]:
-
-
 actions_attr_df.drop_duplicates(subset=['ad_id'], keep='last', inplace=True)
-
-
-# In[52]:
-
-
-actions_attr_df.shape
-
-
-# In[55]:
-
-
-actions_attr_df.columns
-
-
-# In[59]:
-
-
 actions_attr_df.sort_values(by='cpa', inplace=True)
-
-
-# In[63]:
-
-
 actions_attr_df['confidence'].fillna(actions_attr_df.confidence.mean(), inplace=True)
 
 
-# 
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
-# ### I call this part...fun with colors
-# We need to add color names to dataframe in addition to the hex values
-# 
-# -----------------------------------------------
-# -----------------------------------------------
-# -----------------------------------------------
+# # Colors
 
-# ### define color attributes
-
-# In[42]:
+# In[27]:
 
 
 def hex_to_hsv(h):
@@ -761,10 +552,7 @@ def hex_to_hsv(h):
     return h*360, s*100, v/255
 
 
-hex_to_hsv("FFFF00")
-
-
-# In[43]:
+# In[28]:
 
 
 def color_name(h):
@@ -787,7 +575,7 @@ def color_name(h):
     return h
 
 
-# In[44]:
+# In[29]:
 
 
 def color_saturation(h): 
@@ -805,7 +593,7 @@ def color_saturation(h):
     return s
 
 
-# In[45]:
+# In[30]:
 
 
 def color_brightness(h):
@@ -823,63 +611,16 @@ def color_brightness(h):
     return v
 
 
-# In[46]:
-
-
-actions_attr_df['accent_color'].isnull
-
-
-# In[47]:
-
-
-actions_attr_df['accent_color'].mode()[0]
-
-
-# ### Break out accent colors
-
-# In[64]:
-
-
-actions_attr_df['accent_color'] = actions_attr_df['accent_color'].fillna(actions_attr_df['accent_color'].mode()[0])
-
-
-# In[65]:
-
-
-
-
-
-# In[66]:
+# In[32]:
 
 
 actions_attr_df['accent_color_name'] = actions_attr_df['accent_color'].apply(color_name)
-
-
-# In[67]:
-
-
 actions_attr_df['color_saturation'] = actions_attr_df['accent_color'].apply(color_saturation)
 actions_attr_df['color_brightness'] = actions_attr_df['accent_color'].apply(color_brightness)
+actions_attr_df['accent_color'] = actions_attr_df['accent_color'].fillna(actions_attr_df['accent_color'].mode()[0])
 
 
-# In[52]:
-
-
-actions_attr_df.columns
-
-
-# ---------------------------------------
-# ---------------------------------------
-# ---------------------------------------
-# ---------------------------------------
-# ---------------------------------------
-# # Make first tests
-# ---------------------------------------
-# ---------------------------------------
-# ---------------------------------------
-# ---------------------------------------
-
-# In[53]:
+# In[33]:
 
 
 def back_analysis(df):
@@ -888,32 +629,25 @@ def back_analysis(df):
     df = df[0:4]
 
     new_df = df[['sentiment', 'key_phrases_1', 'tone_1', 'tone_2', 'accent_color_name', 'color_saturation', 'color_brightness', 'dominant_color_background', 'body', 'image_url']].copy()
-    
-    return new_df
 
 
-# Index(['dominant_color_background', 'dominant_color_foreground', 'dominant_color_1', 'is_bw_img', 'tags_1', 'caption', 'confidence', 'height', 'width', 'id', 'creative_id', 'sentiment', 'key_phrases_1', 'key_phrases_2', 'key_phrases_3', 'key_phrases_4', 'key_phrases_5', 'tone_1', 'tone_2', 'tone_3', 'tone_4', 'text_sample_1', 'text_sample_2', 'text_sample_3', 'text_sample_4', 'score_1', 'score_2', 'score_3', 'score_4', 'tone_5', 'text_sample_5', 'score_5', 'tone_6', 'score_6', 'text_sample_6', 'accent_color_name', 'color_saturation', 'color_brightness'],
-#       dtype='object')
-
-# In[54]:
+# In[34]:
 
 
 color_var_list = ['accent_color_name', 'color_saturation', 'color_brightness', 'dominant_color_background', 'dominant_color_foreground', 'dominant_color_1']
 text_var_list = ['sentiment', 'tone_1']
 
 
-# In[55]:
+# In[36]:
 
 
-# var_list = ['dominant_color_background', 'dominant_color_foreground', 'dominant_color_1', 'is_bw_img', 'tags_1', 'caption', 'confidence', 'height', 'width', 'sentiment', 'key_phrases_1', 'key_phrases_2', 'key_phrases_3', 'key_phrases_4', 'key_phrases_5', 'tone_1', 'tone_2', 'tone_3', 'tone_4', 'text_sample_1', 'text_sample_2', 'text_sample_3', 'text_sample_4', 'score_1', 'score_2', 'score_3', 'score_4', 'tone_5', 'text_sample_5', 'score_5', 'tone_6', 'score_6', 'text_sample_6', 'accent_color_name', 'color_saturation', 'color_brightness']
 color_variables = pd.DataFrame(actions_attr_df.groupby(color_var_list).cpa.mean())
 color_variables.reset_index(level=color_var_list, inplace=True)
 color_variables.set_index('cpa', inplace=True)
 color_variables.sort_values(by='cpa', inplace=True)
 
 
-
-# In[56]:
+# In[38]:
 
 
 text_variables = pd.DataFrame(actions_attr_df.groupby(text_var_list).cpa.mean())
@@ -922,8 +656,7 @@ text_variables.set_index('cpa', inplace=True)
 text_variables.sort_values(by='cpa', inplace=True)
 
 
-
-# In[57]:
+# In[39]:
 
 
 text_and_color = pd.merge(text_variables, color_variables, on='cpa')
@@ -935,7 +668,7 @@ def get_top_cpas():
     return cpa_trends
 
 
-# In[58]:
+# In[40]:
 
 
 def avg_cpa(df, var, attr):
@@ -945,7 +678,7 @@ def avg_cpa(df, var, attr):
     return index_avg
 
 
-# In[71]:
+# In[41]:
 
 
 def cpa_lookup(df):
@@ -964,7 +697,7 @@ def cpa_lookup(df):
     return lookup_table
 
 
-# In[72]:
+# In[42]:
 
 
 def find_in_lookup(val):
@@ -975,13 +708,7 @@ def find_in_lookup(val):
             return value
 
 
-# In[73]:
-
-
-# find_in_lookup('cyan')
-
-
-# In[167]:
+# In[43]:
 
 
 def est_cpa(df, row_set):
@@ -1001,7 +728,7 @@ def est_cpa(df, row_set):
     return mean
 
 
-# In[261]:
+# In[44]:
 
 
 def petri_dish():
@@ -1034,31 +761,4 @@ def petri_dish():
     #blah.drop_duplicates(subset=['est_cpa'], keep='last', inplace=True)
     
     return blah
-
-
-# In[262]:
-
-
-# blah = petri_dish()
-
-
-# In[263]:
-
-
-# blah.head(20)
-
-
-# In[264]:
-
-
-# blah.shape
-
-
-# In[56]:
-
-
-# def main():
-#     if __name__ == "__main__":
-#         back = back_analysis(actions_attr_df)
-#         petri_dish(back)
 
