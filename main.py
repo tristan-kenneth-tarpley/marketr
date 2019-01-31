bb2c4e376728167451033da5a6227c94fdd0bafd
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2019-01-30 20:08:55 -0600

    working through p1 page

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ import urllib
 import os
 import pyodbc
 import json
+from flask_socketio import SocketIO
 
 import analysis as an
 
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+server = 'tarpley.database.windows.net'
+database = 'marketr'
+username = 'tristan'
+password = 'Fiverrtemp!'
+driver= '{ODBC Driver 13 for SQL Server}'
+
+connStr = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
+db = pyodbc.connect(connStr)
+
+def sql_to_df(x):
+    return pd.read_sql_query(x, db, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
 
 
 #########################
@@ -20,13 +34,55 @@ app = Flask(__name__)
 #########################
 #########################
 
+
+@app.route('/new')
+def new():
+    if not session.get('logged_in'):
+        return render_template('create.html')
+    else:
+        return begin()
+
+
 @app.route('/begin')
 def begin():
-    return render_template('intake/init_setup.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('intake/init_setup.html')
 
-@app.route('/begin/')
-def red_begin():
-    return redirect("/begin", code=302)
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    session['logged_in'] = True 
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    print(POST_USERNAME + " " + POST_PASSWORD)
+
+    cursor = db.cursor()
+
+    query = """INSERT INTO dbo.customer_basic (
+                                    email,
+                                    password)
+                VALUES ('""" + POST_USERNAME + """',
+                        '""" + POST_PASSWORD + """'); commit;"""
+
+    cursor.execute(query)
+
+    cursor.close()
+
+    result = sql_to_df("SELECT * from dbo.customer_basic WHERE email = '" + POST_USERNAME + "' AND password = '" + POST_PASSWORD + "'")
+
+    if result['ID'][0] != None:
+        session['logged_in'] = True
+        session.permanent = True
+        uid = result['ID'][0]
+        session['user'] = int(uid)
+        return begin()
+
+
+
+
 
 @app.route('/competitors', methods=['POST'])
 def competitors():
@@ -36,31 +92,196 @@ def competitors():
     POST_revenue = str(request.form['revenue'])
     POST_city = str(request.form['city'])
     POST_state = str(request.form['state'])
+    POST_stage = str(request.form['one'])
+    POST_employees = str(request.form['employees'])
 
-    print(POST_first_name + " " + POST_last_name + " " + POST_company_name + " " + POST_revenue + " " + POST_city + " " + POST_state)
+
+    query = """UPDATE dbo.customer_basic 
+                SET first_name = '""" + POST_first_name + """', last_name = '""" + POST_last_name + """', company_name = '""" + POST_company_name + """', revenue = '""" + POST_revenue + """', city = '""" + POST_city + """', state = '""" + POST_state + """', stage = '""" + POST_stage + """', employees = '""" + POST_employees + """' 
+                WHERE dbo.customer_basic.ID = '""" + str(session['user']) + """'; commit;"""
+
+    if POST_first_name:
+        cursor = db.cursor()
+        cursor.execute(query)
+        cursor.close()
+    else:
+        print('nothing to see here')
+
+
+    print(POST_stage + " " + POST_first_name + " " + POST_last_name + " " + POST_company_name + " " + POST_revenue + " " + POST_city + " " + POST_state)
+
+    return render_template('intake/competitors.html')
+
+
+
+
+@app.route('/competitors/company', methods=['POST'])
+def company():
+    POST_industry = str(request.form['industry'])
+    POST_comp_1_name = str(request.form['competitor_1_name'])
+    POST_comp_1_website = str(request.form['competitor_1_site'])
+    POST_comp_1_type = str(request.form['comp_1_type'])
+    POST_comp_2_name = str(request.form['competitor_2_name'])
+    POST_comp_2_website = str(request.form['competitor_2_site'])
+    POST_comp_2_type = str(request.form['comp_2_type'])
+
+
+    query = """INSERT INTO dbo.competitors 
+                (customer_id, industry, comp_1_name, comp_1_website, comp_1_type, comp_2_name, comp_2_website, comp_2_type)
+                VALUES ('""" + str(session['user']) + """','""" + POST_industry + """','""" + POST_comp_1_name + """','""" + POST_comp_1_website + """','""" + POST_comp_1_type + """','""" + POST_comp_2_name + """','""" + POST_comp_2_website + """','""" + POST_comp_2_type + """');commit;""" 
+
+    print(query)
+
+    if POST_comp_1_name:
+        cursor = db.cursor()
+        cursor.execute(query)
+        cursor.close()
+    else:
+        print('nothing to see here')
 
     return render_template('intake/company.html')
 
-@app.route('/competitors/')
-def red_competitors():
-    return redirect("/competitors", code=302)
-
-@app.route('/competitors/company')
-def company():
-    return render_template('intake/competitors.html')
-
-@app.route('/competitors/company/')
-def red_company():
-    return redirect('/competitors/company', code=302)
-
-@app.route('/competitors/company/audience')
+@app.route('/competitors/company/audience', methods=['POST'])
 def audience():
+    POST_b2b = str(request.form['b2b'])
+    POST_b2c = str(request.form['b2c'])
+    POST_c2c = str(request.form['c2c'])
+    POST_other = str(request.form['other'])
+    POST_biz_model = str(request.form['biz_model'])
+    POST_storefront = str(request.form['storefront'])
+    POST_direct = str(request.form['direct'])
+    POST_online = str(request.form['online'])
+    POST_tradeshows = str(request.form['tradeshows'])
+    POST_other = str(request.form['other'])
+    POST_freeform = str(request.form['freeform'])
+
+    query = """INSERT INTO dbo.company_info
+                (customer_id, selling_to, selling_to_2, selling_to_3, selling_to_4, biz_model, rev_channel_1, rev_channel_2, rev_channel_3, rev_channel_4, rev_channel_5, rev_channel_freeform)
+                VALUES ('""" + str(session['user']) + """','""" + POST_b2b + """','""" + POST_b2c + """','""" + POST_c2c + """','""" + POST_other + """','""" + POST_biz_model + """','""" + POST_storefront + """','""" + POST_direct + """','""" + POST_online + """','""" + POST_tradeshows + """','""" + POST_other + """','""" + POST_freeform + """');commit;"""
+    print(query)
+    if POST_b2b:
+        cursor = db.cursor()
+        cursor.execute(query)
+        cursor.close()
+    else:
+        print('nothing to see here')
+
     return render_template('intake/audience.html')
 
-@app.route('/competitors/company/audience/')
-def red_audience():
-    return redirect('/competitors/company/audience', code=302)
+@app.route('/competitors/company/audience/product', methods=['POST'])
+def product():
+    POST_gender = str(request.form['gender'])
+    POST_age_group_1 = str(request.form['age_group_1'])
+    POST_age_group_2 = str(request.form['age_group_2'])
+    POST_age_group_3 = str(request.form['age_group_3'])
+    POST_age_group_4 = str(request.form['age_group_4'])
+    POST_age_group_5 = str(request.form['age_group_5'])
+    POST_age_group_6 = str(request.form['age_group_6'])
+    POST_age_group_7 = str(request.form['age_group_7'])
+    POST_age_group_8 = str(request.form['age_group_8'])
+    POST_location = str(request.form['location'])
+    POST_why = str(request.form['why'])
+    POST_before_1 = str(request.form['before_1'])
+    POST_before_2 = str(request.form['before_2'])
+    POST_before_3 = str(request.form['before_3'])
+    POST_before_4 = str(request.form['before_4'])
+    POST_before_5 = str(request.form['before_5'])
+    POST_before_6 = str(request.form['before_6'])
+    POST_before_7 = str(request.form['before_7'])
+    POST_before_8 = str(request.form['before_8'])
+    POST_before_9 = str(request.form['before_9'])
+    POST_before_10 = str(request.form['before_10'])
+    POST_before_freeform = str(request.form['before_freeform'])
+    POST_after_1 = str(request.form['after_1'])
+    POST_after_2 = str(request.form['after_2'])
+    POST_after_3 = str(request.form['after_3'])
+    POST_after_4 = str(request.form['after_4'])
+    POST_after_5 = str(request.form['after_5'])
+    POST_after_6 = str(request.form['after_6'])
+    POST_after_7 = str(request.form['after_7'])
+    POST_after_8 = str(request.form['after_8'])
+    POST_after_9 = str(request.form['after_9'])
+    POST_after_10 = str(request.form['after_10'])
+    POST_after_freeform = str(request.form['after_freeform'])
 
+    query = """INSERT INTO dbo.audience
+                (customer_id, gender, age_group_1, age_group_2, age_group_3, age_group_4, age_group_5, age_group_6, age_group_7, age_group_8, location, why, before_1, before_2, before_3, before_4, before_5, before_6, before_7, before_8, before_9, before_10, before_freeform, after_1, after_2, after_3, after_4, after_5, after_6, after_7, after_8, after_9, after_10, after_freeform)
+                VALUES ('""" + str(session['user']) + """','""" + POST_gender + """','""" + POST_age_group_1 + """','""" + POST_age_group_2 + """','""" + POST_age_group_3 + """','""" + POST_age_group_4 + """','""" + POST_age_group_5 + """','""" + POST_age_group_6 + """','""" + POST_age_group_7 + """','""" + POST_age_group_8 + """','""" + POST_location + """','""" + POST_why + """','""" + POST_before_1 + """','""" + POST_before_2 + """','""" + POST_before_3 + """','""" + POST_before_4 + """','""" + POST_before_5 + """','""" + POST_before_6 + """','""" + POST_before_7 + """','""" + POST_before_8 + """','""" + POST_before_9 + """','""" + POST_before_10 + """','""" + POST_before_freeform + """','""" + POST_after_1 + """','""" + POST_after_2 + """','""" + POST_after_3 + """','""" + POST_after_4 + """','""" + POST_after_5 + """','""" + POST_after_6 + """','""" + POST_after_7 + """','""" + POST_after_8 + """','""" + POST_after_9 + """','""" + POST_after_10 + """','""" + POST_after_freeform + """');commit;"""
+
+    if POST_gender:
+        cursor = db.cursor()
+        cursor.execute(query)
+        cursor.close()
+    else:
+        print('nothing to see here!')
+
+    return render_template('/intake/product.html')
+
+
+@app.route('/competitors/company/audience/product/product_2', methods=['POST'])
+def product_2():
+    POST_gen_description = str(request.form['gen_description'])
+    POST_quantity = str(request.form['quantity'])
+    POST_sku = str(request.form['sku'])
+    POST_link = str(request.form['link'])
+    POST_segment_1 = str(request.form['segment_1'])
+    POST_segment_2 = str(request.form['segment_2'])
+    POST_segment_3 = str(request.form['segment_3'])
+    POST_segment_4 = str(request.form['segment_4'])
+    POST_segment_5 = str(request.form['segment_5'])
+    POST_segment_6 = str(request.form['segment_6'])
+    POST_segment_7 = str(request.form['segment_7'])
+    POST_segment_8 = str(request.form['segment_8'])
+    POST_segment_9 = str(request.form['segment_9'])
+    POST_segment_10 = str(request.form['segment_10'])
+    POST_source_1 = str(request.form['source_1'])
+    POST_source_2 = str(request.form['source_2'])
+    POST_source_3 = str(request.form['source_3'])
+    POST_source_4 = str(request.form['source_4'])
+    POST_source_freeform = str(request.form['source_freeform'])
+
+
+
+    query = """INSERT INTO dbo.product_gen
+                (uid,gen_description,quantity,sku,link,segment_1,segment_2,segment_3,segment_4,segment_5,segment_6,segment_7,segment_8,segment_9,segment_10,source_1,source_2,source_3,source_4,source_freeform)
+                VALUES ('""" + str(session['user']) + """','""" + POST_gen_description + """','""" + POST_quantity + """','""" + POST_sku + """','""" + POST_link + """','""" + POST_segment_1 + """','""" + POST_segment_2 + """','""" + POST_segment_3 + """','""" + POST_segment_4 + """','""" + POST_segment_5 + """','""" + POST_segment_6 + """','""" + POST_segment_7 + """','""" + POST_segment_8 + """','""" + POST_segment_9 + """','""" + POST_segment_10 + """','""" + POST_source_1 + """','""" + POST_source_2 + """','""" + POST_source_3 + """','""" + POST_source_4 + """','""" + POST_source_freeform + """');commit;"""
+       
+
+
+    if POST_sku:
+        cursor = db.cursor()
+        cursor.execute(query)
+        cursor.close()
+
+    else:
+        print("seriously...NOTHING TO SEE HERE") 
+
+    d = request.form
+
+    new_df = pd.DataFrame(list(d.items()))
+    new_df = new_df[20:]
+    
+    print(new_df.head(40))
+
+    for
+
+    POST_product_len = request.form['product_len']
+    
+    # q = 1
+    # while q < POST_product_len:
+    #     #cursor = db.cursor()
+    #     working = "INSERT INTO dbo.product_list (name, category, cogs, sales_price, qty_sold, est_unique_buyers) VALUES ('" + 
+    #                                                                                                                                             """name="product_` + i + `"_name"""
+
+    return render_template('/intake/product_2.html')
+
+@app.route('/competitors/company/audience/product/product_2/salescycle', methods=['POST'])
+def salescycle():
+    return render_template('/intake/salescycle.html')
+
+@app.route('/company_info', methods=['POST'])
+def company_info():
+    return nice()
 
 @app.route('/nice')
 def nice():
@@ -79,11 +300,45 @@ def red_nice():
 def goals():
     return render_template('intake/2/goals.html')
 
+
+
+
+
+
+
+
+
+@app.route('/begin/')
+def red_begin():
+    return redirect("/begin", code=302)
+
+@app.route('/competitors/')
+def red_competitors():
+    return redirect("/competitors", code=302)
+
+@app.route('/competitors/company/')
+def red_company():
+    return redirect('/competitors/company', code=302)
+
+@app.route('/competitors/company/audience/')
+def red_audience():
+    return redirect('/competitors/company/audience', code=302)
+
+@app.route('/competitors/company/audience/product/')
+def red_product():
+    return redirect('/competitors/company/audience/product', code=302)
+
+@app.route('/competitors/company/audience/product/salescycle/')
+def red_salescycle():
+    return redirect('/competitors/company/audience/product/salescycle', code=302)
+
+
+@app.route('/nice')
+
+
 @app.route('/goals/')
 def red_goals():
     return redirect('/goals', code=302)
-
-
 
 
 #########################
@@ -103,12 +358,14 @@ def do_admin_login():
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
  
-    result = an.sql_to_df("SELECT * from dbo.customers WHERE email = '" + POST_USERNAME + "' AND password = '" + POST_PASSWORD + "'")
+    result = an.sql_to_df("SELECT * from dbo.customer_basic WHERE email = '" + POST_USERNAME + "' AND password = '" + POST_PASSWORD + "'")
     try:    
-        if result['id'][0] != None:
+        if result['ID'][0] != None:
             session['logged_in'] = True
+            uid = result['ID'][0]
+            session['user'] = int(uid)
             session.permanent = True
-            return index()
+            return begin()
 
     except IndexError:
         flash('wrong password!')
@@ -122,32 +379,10 @@ def logout():
     return index()
 
 
-@app.route('/new')
-def new():
-    if not session.get('logged_in'):
-        return render_template('create.html')
-    else:
-        return render_template('index.html')
 
 
-@app.route('/create_user')
-def create_user():
-    session['logged_in'] = True 
-    POST_USERNAME = str(request.form['username'])
-    POST_PASSWORD = str(request.form['password'])
 
 
-    query = """INSERT INTO dbo.customers (
-                                email,
-                                password)
-            VALUES ('""" + str(POST_USERNAME) + """',
-                    '""" + str(POST_PASSWORD) + """'; commit;"""
-
-    cursor = an.cursor
-
-    cursor.execute(query)
-
-    return index()
 
 
 
--- static/animations.js
+++ static/animations.js
@@ -2,20 +2,52 @@ $('.hover_box').click(function(){
 
 
 	if($(this).hasClass('hb_many')){
-
 		$(this).toggleClass('hover_box_selected')
-
 	} else {
-
 		$(this).toggleClass('hover_box_selected')
 		$(this).siblings().removeClass('hover_box_selected')
 		$(this).parent().siblings().children().removeClass('hover_box_selected')
+	}
 
+	var test = $(this).find("h6")
+	var text = test[0]['textContent']
 
+	if ($(this).hasClass('hb_many')){
+		var nearest_input = $(this).find('.hidden_input');
+	} else if ($(this).hasClass('multi_row')){
+		var nearest_input = $(this).parentsUntil('.grandparent').find('.hidden_input')
+		console.log(nearest_input)
+	} else {
+		var nearest_input = $(this).parent().find('.hidden_input');
+	}
+
+	if ($(this).hasClass('hover_box_selected')){	
+		nearest_input.val(text)
+	} else {
+		nearest_input.val("")
 	}
 
 
 })
+
+var i = 1
+$(".edit_products").click(function(e){
+
+
+	if ($(this).hasClass("add_product")){
+		i++
+		$('.input_table').append("<tr class='table_row'><td><input type='text' name='product_" + i + "_name' placeholder='product name " + i + "'></td><td><input type='text' name='p" + i + "_category' placeholder='details'></td><td><input type='text' name='p" + i + "_cogs' placeholder='$0.00'></td><td><input type='text' name='p" + i + "_sales_price' placeholder='$0.00'></td><td><input type='text' name='p" + i + "_qty_sold' placeholder='0'></td><td><input type='text' name='p" + i + "_est_unique_buyers' placeholder='0'></td><tr>")
+	} else if ($(this).hasClass('remove_product')){
+		if (i > 1){
+			i--
+		}
+		var last = $('.table_row').last()
+		last.remove()
+	}
+	$('.product_len').val(i)
+		
+})
+
 
 
 $(document).ready(function(){
@@ -28,3 +60,14 @@ $(document).ready(function(){
 	$('.step').css('width', width+"%")
 
 })
+
+
+
+
+
+
+
+
+
+
+
