6126e2387fb66c03506a702c4859c8559250f247
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2019-10-10 11:04:55 -0500

    changed spend validation

--- static/core-js/ViewModels.js
+++ static/core-js/ViewModels.js
@@ -432,9 +432,7 @@ const WalletViewModel = class {
 
     add_validation(){
         $(".almost_free_val").keyup(e=>{
-            console.log($(".almost_free_val").val())
             let $this = parseInt($(".almost_free_val").val())
-            console.log($this)
             if ($this > 499) {
                 this.disable('almost_free')
             } else {
@@ -443,9 +441,8 @@ const WalletViewModel = class {
         })
 
         $(".ad_mid_val").keyup(e=>{
-            console.log($(".ad_mid_val").val())
+
             let $this = parseInt($(".ad_mid_val").val())
-            console.log($this)
             if ($this > 4999) {
                 this.disable('mid')
             } else if ($this < 250) {
