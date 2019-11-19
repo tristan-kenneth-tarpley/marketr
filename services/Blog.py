e4dd44353069b888c6f9b3bf9f5f287eda5141fc
Tristan Kenneth Tarpley <tristan@tarpleyholdings.com>
2019-11-19 16:02:13 -0600

    segmented out api keys

--- services/Blog.py
+++ services/Blog.py
@@ -1,17 +1,20 @@
 import requests
+from app import app
 
 class Blog:
     def __init__(self):
         self.endpoint = "https://api.storyblok.com/v1/cdn/stories"
+        app.config.from_pyfile('config.cfg')
+        self.key = app.config['BLOG']
 
     def single_story(self, slug):
         url = f"{self.endpoint}/posts/{slug}"
-        querystring = {"token":"J0rjehqcQuHUBmQziiJgLAtt", "version": "published"}
+        querystring = {"token":self.key, "version": "published"}
         return self.fetch(url, querystring)
 
     def all_stories(self):
         url = self.endpoint
-        querystring = {"token":"J0rjehqcQuHUBmQziiJgLAtt", "starts_with":"posts/","cv": 1}
+        querystring = {"token":self.key, "starts_with":"posts/","cv": 1}
         return self.fetch(url, querystring)
 
     def fetch(self, url, querystring):
--- services/CampaignsService.py
+++ services/CampaignsService.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine
+from app import app
+
+class CampaignData:
+    def __init__(self):
+        app.config.from_pyfile('config.cfg')
+        postgres_str = f'postgresql://{app.config['POSTGRES_USERNAME']}:{app.config['POSTGRES_PASSWORD']}@{app.config['POSTGRES_ADDRESS']}:{app.config['POSTGRES_PORT']}/{app.config['POSTGRES_DBNAME']}'
+        self.cnx = create_engine(postgres_str)
+
+    
