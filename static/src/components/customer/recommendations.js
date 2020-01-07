e3e9cc269e5349f9738c1aba42cf56601f18a589
Tristan Tarpley <tristan@marketr.life>
2020-01-07 00:45:03 -0600

    can't get this freaking nested custom elements to render with proper attributes

--- static/src/components/customer/recommendations.js
+++ static/src/components/customer/recommendations.js
@@ -1,3 +1,5 @@
+import Rec_shell from '/static/src/components/customer/rec_shell.js'
+
 const styles = () => {
     /*html*/
     return `
@@ -5,6 +7,29 @@ const styles = () => {
         @import url('/static/assets/css/bootstrap.min.css');
         @import url('/static/assets/css/styles.css');
         @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+        .rec-container {
+            padding: 1%;
+        }
+        .rec {
+            border-left: 4px solid gray;
+            margin-bottom: 2%;
+            padding: 5% 2% 0 5%;
+        }
+        .dismiss {
+            position: relative;
+            right: 15%;
+            top: 10%;
+        }
+        .rec-title {
+            line-height: .5em;
+        }
+        .rec-apply {
+            font-size: 75%;
+            /*float: right;*/
+        }
+        .read-more {
+            margin: auto;
+        }
     </style>
     `.trim()
 }
@@ -24,15 +49,40 @@ export default class Recommendations extends HTMLElement {
         this.css = styles()
     }
 
+    recommendation(rec, index){
+        const el = document.createElement('recommendation-shell')
+        el.setAttribute('rec_id', rec.rec_id)
+        el.setAttribute('index', index)
+        el.setAttribute('customer_id', this.customer_id)
+        el.setAttribute('title', rec.title)
+        el.setAttribute('body', rec.body)
+
+        return el
+    }
+
     render(){
         this.shadow.innerHTML = ""
-        const el = document.createElement('div')
-        /*html */
-        el.innerHTML = `
-            ${this.css}
-            <p>Hello world</p>
-        `
-        this.shadow.appendChild(el);
+        let el = document.createElement('div')
+
+        fetch('/api/outstanding_recs', {
+            method: 'POST',
+            headers : new Headers({
+                "content-type": "application/json"
+            }),
+            body:  JSON.stringify({
+                customer_id: this.customer_id,
+                admin_id: this.admin_id
+            })
+        })
+            .then(res=>res.json())
+            .then( res=> this.state.data = res )
+            .then(res=>{
+                /*html */
+                el.innerHTML = `${this.css}`
+
+                for (let i in res) el.appendChild(this.recommendation(res[i], i))
+            })
+            .then(this.shadow.appendChild(el))
     }
 
     connectedCallback(){
